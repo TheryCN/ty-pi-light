435738bcdba755d206f81a57880393eaf901960b
Théry Charasson <thery.charasson@gmail.com>
2019-01-18 22:30:05 +0100

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -7,7 +7,6 @@ import unicornhat as unicorn
 
 def main():
     try:
-        print(UIColor.convert(11.5))
         unicorn.set_layout(unicorn.AUTO)
         unicorn.rotation(0)
         unicorn.brightness(0.3)
@@ -16,17 +15,18 @@ def main():
         predictions = [UIColor.convert(0), UIColor.convert(3), UIColor.convert(4), UIColor.convert(2),
         UIColor.convert(2), UIColor.convert(5), UIColor.convert(8), UIColor.convert(12)]
 
-        for y in range(height):
+        for x in range(width):
             rgb = predictions[y]
-            for x in range(width):
+            for y in range(height):
                 unicorn.set_pixel(x, y, rgb[0], rgb[1], rgb[2])
                 unicorn.show()
                 time.sleep(0.05)
 
-        time.sleep(100)
     except:
         print('traceback.format_exc():\n%s',traceback.format_exc())
         exit()
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
+        time.sleep(60*10)
